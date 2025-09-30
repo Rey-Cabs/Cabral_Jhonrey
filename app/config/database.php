2426b5d3cc35f9ba36e3f4375a9581eba9183697
Jhonrey Cabral <jhonreycabral@gmail.com>
2025-09-30 13:36:53 +0800

    new db

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12799589',
-    'password'	=> 'uysb6KLj1Z',
-    'database'	=> 'sql12799589',
+    'username'	=> 'sql12800691',
+    'password'	=> 'wC381wVaAR',
+    'database'	=> 'sql12800691',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
